91a91197df89ef7107f8aebf6ce0a6369cf044b1
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-01-23 23:56:39 -0600

    Fix issues with Alleviating

--- src/main/java/com/minecraftabnormals/allurement/core/mixin/ExperienceOrbEntityMixin.java
+++ src/main/java/com/minecraftabnormals/allurement/core/mixin/ExperienceOrbEntityMixin.java
@@ -1,17 +1,15 @@
 package com.minecraftabnormals.allurement.core.mixin;
 
 import com.minecraftabnormals.allurement.core.registry.AllurementEnchantments;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.item.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Map;
 
 @Mixin(ExperienceOrbEntity.class)
 public class ExperienceOrbEntityMixin {
@@ -21,9 +19,11 @@ public class ExperienceOrbEntityMixin {
 
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;onItemPickup(Lnet/minecraft/entity/Entity;I)V", shift = At.Shift.AFTER), method = "onCollideWithPlayer", cancellable = true)
 	private void onCollideWithPlayer(PlayerEntity player, CallbackInfo ci) {
-		Map<EquipmentSlotType, ItemStack> map = AllurementEnchantments.ALLEVIATING.get().getEntityEquipment(player);
-		if (!map.isEmpty()) {
-			float i = Math.min(this.xpValue * 0.25F * map.size(), player.getMaxHealth() - player.getHealth());
+		int count = 0;
+		for (EquipmentSlotType slot : EquipmentSlotType.values())
+			count += EnchantmentHelper.getEnchantmentLevel(AllurementEnchantments.ALLEVIATING.get(), player.getItemStackFromSlot(slot));
+		if (count > 0) {
+			float i = Math.min(this.xpValue * 0.25F * count, player.getMaxHealth() - player.getHealth());
 			this.xpValue -= i * 4.0F;
 			player.heal(i);
 		}
