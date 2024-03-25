ead58f8509c92e270e4440c8ced64e4fbdba4e25
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-03-25 16:08:15 -0500

    Fix Chiseled Bookshelf config not working and particles not working; Fixes #51

--- src/main/java/com/teamabnormals/allurement/core/mixin/ChiseledBookShelfBlockMixin.java
+++ src/main/java/com/teamabnormals/allurement/core/mixin/ChiseledBookShelfBlockMixin.java
@@ -1,5 +1,6 @@
 package com.teamabnormals.allurement.core.mixin;
 
+import com.mojang.datafixers.TypeRewriteRule.All;
 import com.teamabnormals.allurement.core.AllurementConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.ItemTags;
@@ -10,7 +11,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ChiseledBookShelfBlock;
 import net.minecraft.world.level.block.entity.ChiseledBookShelfBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.stream.Stream;
 
 @Mixin(ChiseledBookShelfBlock.class)
 public abstract class ChiseledBookShelfBlockMixin extends Block {
@@ -21,19 +25,33 @@ public abstract class ChiseledBookShelfBlockMixin extends Block {
 
 	@Override
 	public float getEnchantPowerBonus(BlockState state, LevelReader level, BlockPos pos) {
-		if (!level.isClientSide() && level.getBlockEntity(pos) instanceof ChiseledBookShelfBlockEntity blockEntity) {
-			int books = 0;
-			int enchantedBooks = 0;
-			for (int i = 0; i < blockEntity.getContainerSize(); i++) {
-				ItemStack stack = blockEntity.getItem(i);
-				if (stack.is(Items.ENCHANTED_BOOK)) {
-					enchantedBooks++;
-				} else if (stack.is(ItemTags.BOOKSHELF_BOOKS)) {
-					books++;
+		if (AllurementConfig.COMMON.chiseledBookshelfEnchanting.get()) {
+			if (!level.isClientSide()) {
+				if (level.getBlockEntity(pos) instanceof ChiseledBookShelfBlockEntity blockEntity) {
+					int books = 0;
+					int enchantedBooks = 0;
+					for (int i = 0; i < blockEntity.getContainerSize(); i++) {
+						ItemStack stack = blockEntity.getItem(i);
+						if (stack.is(Items.ENCHANTED_BOOK)) {
+							enchantedBooks++;
+						} else if (stack.is(ItemTags.BOOKSHELF_BOOKS)) {
+							books++;
+						}
+					}
+
+					return ((float) books) / AllurementConfig.COMMON.booksNeededPerLevel.get() + ((float) enchantedBooks) / AllurementConfig.COMMON.enchantedBooksNeededPerLevel.get();
+				}
+			} else {
+				int count = 0;
+				for (BooleanProperty property : ChiseledBookShelfBlock.SLOT_OCCUPIED_PROPERTIES) {
+					if (state.getValue(property)) {
+						count++;
+					}
+				}
+				if (count > 0) {
+					return count / 3.0F;
 				}
 			}
-
-			return ((float) books) / AllurementConfig.COMMON.booksNeededPerLevel.get() + ((float) enchantedBooks) / AllurementConfig.COMMON.enchantedBooksNeededPerLevel.get();
 		}
 		return super.getEnchantPowerBonus(state, level, pos);
 	}
